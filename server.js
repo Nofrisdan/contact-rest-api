2ba1170fef4e4dbfd0ad5b28a0206e5b0fe62f56
Nofrisdan <nofrisdansitopu22@gmail.com>
2022-02-02 18:09:00 +0700

    Menambahkan Cors pada server

--- server.js
+++ server.js
@@ -3,16 +3,17 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+const cors = require("cors");
 // parser
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 
+// cors
+app.use(cors());
+
 // routes
 const routes = require("./routes");
 routes(app);
-
-
 
 // configuration
 app.listen(port,() => {
