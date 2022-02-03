baf26667e3c69081b9f334b818014595d84de9b5
Nofrisdan <nofrisdansitopu22@gmail.com>
2022-02-04 01:07:00 +0700

    Modifikasi server.js

--- server.js
+++ server.js
@@ -4,17 +4,22 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const cors = require("cors");
+const morgan = require("morgan");
+
 // parser
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 
 // cors
 app.use(cors());
+app.use(morgan("dev"));
 
-// routes
+// routes middleware
+app.use("/auth",require("./middleware"));
+
+// routes utama
 const {routesMysql} = require("./routes");
 routesMysql(app);
-
 
 
 // configuration
